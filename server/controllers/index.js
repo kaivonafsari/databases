4a9b094237c6810bd9bed1df25fc50e5e34dcf7f
kaivonafsari <kaivon.afsari@gmail.com>
2015-04-18 12:43:42 -0700

    complete part 1

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,94 +1,57 @@
 var models = require('../models');
 var bluebird = require('bluebird');
 var db = require('../db');
-var qs = require('querystring');
+
 
 
 module.exports = {
   messages: {
     get: function (req, res) {
-      console.log("GET handler called")
-      db.connection.query('SELECT * FROM `messages`',function(err, results) {
-        console.log("SELECT DB query ran.")
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(results));
-
+      console.log("ENTERED message GET HANDLER " + JSON.stringify(req.body));
+      models.messages.get(function(err, results){
+        if (err) {
+          res.status(500).send("we fucked up");
+        } else {
+          res.status(200).send(JSON.stringify(results));
+        }
       })
-
     }, // a function which handles a get request for all messages
     post: function (req, res) {
-      request.body
-      // console.log("Messages POST handler called")
-      // var body = '';
-      // req.on('data', function (data) {
-      //     body += data;
+      console.log("WE GOT TO THE POST "+ JSON.stringify(req.body));
 
-      // });
-      // req.on('end', function () {
-      //     var post = JSON.parse(body);
-      //     db.connection.query({
-      //       sql: 'INSERT INTO messages (username,roomname,message) values (?,?,?)',
-      //       timeout: 40000, // 40s
-      //       values: [post.username,post.roomname,post.message]
-      //     }, function (error, results, fields) {
-      //       console.log("INSERT DB query ran.");
-      //      if (error) {
-      //        res.writeHead(500,null);
-      //        res.end();
-      //      } else {
-      //       console.log("Ending Messages POST response.")
-      //        res.writeHead(201,null);
-      //        res.end();
-      //      }
-      //     });
-
-      //     // use post['blah'], etc.
-      // });
-
+      models.messages.post(req.body, function(err, results){
+        if (err) {
+          res.status(500).send("sorry");
+        } else {
+          console.log("RBM: " + JSON.stringify(req.body.message));
+          res.status(201).send([{text: JSON.stringify(req.body.message)}]);
+        }
+      })
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
-      console.log("GET handler called")
-      db.connection.query('SELECT * FROM `users`',function(err, results) {
-        console.log("SELECT DB query ran.")
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify(results));
-
+      console.log("ENTERED USER GET HANDLER " + JSON.stringify(req.body));
+      models.users.get(function(err, results){
+        if (err) {
+          res.status(500).send("we fucked up");
+        } else {
+          res.status(200).send(JSON.stringify(results));
+        }
       })
-
     },
     post: function (req, res) {
-      console.log("Users POST handler called")
-      var body = '';
-      req.on('data', function (data) {
-          body += data;
-          console.log("Recieved data!");
-      });
-      req.on('end', function () {
-        console.log("POST data collection finished.")
-          var post = JSON.parse(body);
-          db.connection.query({
-            sql: 'INSERT INTO users (username) values (?)',
-            timeout: 40000, // 40s
-            values: [post.username]
-          }, function (error, results, fields) {
-            console.log("INSERT DB query ran.");
-           if (error) {
-             res.writeHead(500,null);
-             res.end();
-           } else {
-            console.log("Ending Users POST response.")
-             res.writeHead(201,null);
-             res.end();
-           }
-          });
+        console.log("USER POST "+ JSON.stringify(req.body));
 
-          // use post['blah'], etc.
-      });
+        models.users.post(req.body, function(err, results){
+          if (err) {
+            res.status(500).send("sorry");
+          } else {
+            res.status(201).send();
+          }
+        })
+      } // a function which handles posting a message to the database
     }
-  }
 };
-
--- server/models/index.js
+++ server/models/index.js
@@ -5,14 +5,41 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (callback) {
+      db.connection.query('SELECT * FROM `messages`',function(err, results) {
+        callback(err, results);
+
+      })
+    }, // a function which produces all the messages
+    post: function (data, callback) {
+
+      db.connection.query({
+              sql: 'INSERT INTO messages (username,roomname,message) values (?,?,?)',
+              timeout: 4000, // 4s
+              values: [data.username,data.roomname,data.message]
+            }, function (err, results, fields) {
+              callback(err,results);
+            });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      db.connection.query('SELECT * FROM `users`',function(err, results) {
+        callback(err, results);
+
+      })
+    },
+    post: function (data,callback) {
+      db.connection.query({
+              sql: 'INSERT INTO users (username) values (?)',
+              timeout: 4000, // 4s
+              values: [data.username]
+            }, function (err, results, fields) {
+              callback(err,results);
+            });
+    }
   }
 };
 
