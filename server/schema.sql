2717b5af4999166d008cee93e986c4e013dba6ac
kaivonafsari <kaivon.afsari@gmail.com>
2015-04-17 17:20:37 -0700

    create default schema for database

--- server/schema.sql
+++ server/schema.sql
@@ -2,9 +2,33 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NULL AUTO_INCREMENT DEFAULT NULL,
+  `username` VARCHAR(25) NULL DEFAULT 'anonymous',
+  `roomname` VARCHAR(25) NULL DEFAULT 'main',
+  `message` VARCHAR(140) DEFAULT 'no content',
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Foreign Keys
+-- ---
+
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ---
+-- Test Data
+-- ---
+
+INSERT INTO `messages` (`id`,`username`,`roomname`,`message`) VALUES
+('1','Kyle and Kaivon','main','ayy lmao');
+
+
 
 /* Create other tables and define schemas for them here! */
 
